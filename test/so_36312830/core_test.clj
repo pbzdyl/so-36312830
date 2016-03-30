47d31a2e1c2e28571e1bbbf5740fbf59287ab74f
Piotrek Bzdyl <piotr@bzdyl.net>
2016-03-30 19:37:11 +0200

    Changed example to show overriding `extend-protocol` for the same type.

--- test/so_36312830/core_test.clj
+++ test/so_36312830/core_test.clj
@@ -3,9 +3,9 @@
             [so-36312830.core :refer :all]))
 
 (extend-protocol IPrintable
-  clojure.lang.IPersistentMap
-  (prnt [this] (str "clojure.lang.IPersistentMap " this)))
+  clojure.lang.Associative
+  (prnt [this] (str "My custom impl for clojure.lang.Associative " this)))
 
 (deftest override-extend-protocol
   (println (prnt {:a 1}))
-  (is (= "clojure.lang.IPersistentMap {:a 1}" (prnt {:a 1}))))
+  (is (= "My custom impl for clojure.lang.Associative {:a 1}" (prnt {:a 1}))))
